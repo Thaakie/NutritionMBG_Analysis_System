11762f53b69ccb69c63a2610f6acbfe8b103fb0d
Neemonzy <neemonzy@gmail.com>
2026-04-20 18:44:58 +0700

    update ulang project

--- frontend/src/utils/formatters.js
+++
@@ -1,9 +0,0 @@
-export function formatNumber(value) {
-  return new Intl.NumberFormat("id-ID", {
-    maximumFractionDigits: 2,
-  }).format(Number(value || 0));
-}
-
-export function formatPercent(value) {
-  return `${formatNumber(value)}%`;
-}
